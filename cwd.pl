3189e7b54c403368d2786807ee9232a61c0dfec5
mapps-fujisawa <yoshitaka.fujisawa@mkt-apps.com>
2016-10-07 02:34:26 +0900

    file tidy

--- cwd.pl
+++
@@ -1,7 +0,0 @@
-#use common::sense;
-#use Cwd qw(abs_path);
-BEGIN {require Cwd; Cwd->import('abs_path');};
-
-#use Data::Dumper;
-
-
--- xslate/use.pl
+++
@@ -1,8 +0,0 @@
-use Text::Xslate;
-my $tx       = Text::Xslate->new();
-my $template = 'Hello, <: $lang :> world!';
-
-my $result = $tx->render_string( $template, { lang => 'Template' }, );
-
-print $result;
-# => 'Hello, Template world!'
